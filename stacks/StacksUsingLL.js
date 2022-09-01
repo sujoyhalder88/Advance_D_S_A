a160067cdf63931db00f53609ee5789dcb33edf5
sujoyhalder88 <sujoyhalder88@gmail.com>
2022-09-01 17:33:59 +0530

    add

--- stacks/StacksUsingLL.js
+++ stacks/StacksUsingLL.js
@@ -79,10 +79,20 @@ class Stack {
 }
 
 let st = new Stack();
+// st.push(1);
+// st.push(2);
+// st.push(3);
+// st.push(4);
+// console.log(st.top());
+// st.pop();
+// console.log(st.top());
 st.push(1);
+st.pop();
 st.push(2);
 st.push(3);
-st.push(4);
-console.log(st.top());
 st.pop();
+st.push(4);
+st.pop();
+st.pop();
+st.push(5);
 console.log(st.top());
